83bddbd8ab4d0c9bf4261af69f3c78d6a3ce3dc6
vanitha06 <vinuharan@yahoo.co.in>
2026-04-19 19:21:02 +0000

    second try - training VLM - 70% accuracy

--- homework4/homework/finetune.py
+++ homework4/homework/finetune.py
@@ -111,11 +111,11 @@ def train(
     data_dir: Path | None = None,
     train_dataset_name: str = "train",
     output_dir: str = "vlm_sft",
-    num_train_epochs: int = 0.05,  # use only 0.05 epoch for training
+    num_train_epochs: int = 0.08,  # use only 0.05 epoch for training
     per_device_train_batch_size: int = 8,
-    gradient_accumulation_steps: int = 32,
-    learning_rate: float = 2e-4,
-    lora_r: int = 4,
+    gradient_accumulation_steps: int = 4,
+    learning_rate: float = 5e-4,
+    lora_r: int = 16,
     lora_alpha: int = 32,
     lora_dropout: float = 0.0,
     num_workers: int = 16,
